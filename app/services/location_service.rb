6cd8a5466fe663091d8c167263c6c8f8e03181a4
Tim Allen <tnelsallen3@gmail.com>
2019-04-13 18:54:01 -0600

    Finished request spec

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,19 +1,11 @@
 class LocationService
   def self.get_forecast_for_location(location)
-    fetch_data(location)
+    coordinate_query = geocoding_service.find_lat_and_long(location)
+    forecast_data = dark_sky_service.get_forecast(coordinate_query)
+    location_formatter.format_location(coordinate_query, forecast_data)
   end
 
   private
-
-  def self.fetch_data(location)
-    coordinate_query = geocoding_service.find_lat_and_long(location)
-    forecast_data = dark_sky_service.get_forecast(coordinate_query)
-    package_data(coordinate_query, forecast_data)
-  end
-
-  def self.package_data(coordinate_query, forecast_data)
-    location_formatter.format_location(coordinate_query, forecast_data)
-  end
 
   def self.geocoding_service
     GeocodingService.new('https://maps.googleapis.com')
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -5,5 +5,14 @@ describe 'Forecast API' do
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response_body).to be_a(Hash)
+    expect(response_body[:location]).to be_a(String)
+    expect(response_body[:country]).to be_a(String)
+    expect(response_body[:time]).to be_a(String)
+    expect(response_body[:current_weather_info]).to be_a(Hash)
+    expect(response_body[:hourly_data]).to be_a(Array)
+    expect(response_body[:daily_data]).to be_a(Array)
   end
 end
