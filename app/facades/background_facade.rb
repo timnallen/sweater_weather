a2531dd7b546c1dbc259b0294986f4b7a9658dbf
Tim Allen <tnelsallen3@gmail.com>
2019-04-17 10:28:58 -0600

    refactor background_facade

--- app/facades/background_facade.rb
+++ app/facades/background_facade.rb
@@ -1,10 +1,16 @@
 class BackgroundFacade
-  def get_location_background(location)
+  def initialize(location)
+    @location = location
+  end
+
+  def get_location_background
     background_data = service.get_background(location)
     Background.new(background_data)
   end
 
   private
+
+  attr_reader :location
 
   def service
     BackgroundService.new('https://api.unsplash.com')
